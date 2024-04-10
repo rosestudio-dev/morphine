9a217f0efb0c2c0cbf5bcdf464153465e5adb318
why-iskra <v_iskortsev@mail.ru>
2024-04-10 08:24:54 +0300

    fix aux

--- components/morphine/include/morphine/auxiliary/checkargs.h
+++ components/morphine/include/morphine/auxiliary/checkargs.h
@@ -7,8 +7,4 @@
 #include "morphine/platform.h"
 #include "morphine/config.h"
 
-MORPHINE_AUX void maux_checkargs_fixed(morphine_coroutine_t, size_t count);
 MORPHINE_AUX size_t maux_checkargs(morphine_coroutine_t, size_t count, ...);
-MORPHINE_AUX void maux_checkargs_self(morphine_coroutine_t, size_t count);
-MORPHINE_AUX size_t maux_checkargs_or(morphine_coroutine_t, size_t count1, size_t count2);
-MORPHINE_AUX size_t maux_checkargs_minimum(morphine_coroutine_t, size_t minimum);
