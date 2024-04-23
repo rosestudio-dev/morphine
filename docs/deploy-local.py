744fe8da459dab632135ebfc1b8abf4a24e0a76b
why-iskra <v_iskortsev@mail.ru>
2024-04-23 21:10:44 +0300

    local deploy

---
+++ docs/deploy-local.py
@@ -0,0 +1,32 @@
+import shutil
+import subprocess
+
+gradle = shutil.which("gradle")
+npm = shutil.which("npm")
+
+if gradle is None:
+    print("Require gradle")
+    exit(1)
+
+if npm is None:
+    print("Require npm")
+    exit(1)
+
+
+def step(name, args, cwd):
+    print("\u001b[32;1m-- " + name + " --\u001b[0m")
+
+    code = subprocess.call(
+        args=args,
+        cwd=cwd,
+        stderr=subprocess.STDOUT
+    )
+
+    if code != 0:
+        print("\u001b[31;1m-- Failure --\u001b[0m")
+        exit(1)
+
+
+step("Install deps", [npm, "i"], "site")
+step("Generate docs", [gradle, "run", "--args", "../../components ../site/generated kt c"], "gen")
+step("Deploy", [npm, "run", "docs:dev"], "site")
