d6a59cafd50db401319a76c9a79ef5d302ca18b1
why-iskra <v_iskortsev@mail.ru>
2024-04-27 09:31:22 +0300

    improve native builder

--- components/morphine/include/morphine/auxiliary/nativebuilder.h
+++ components/morphine/include/morphine/auxiliary/nativebuilder.h
@@ -7,9 +7,10 @@
 #define nb_function(U) morphine_coroutine_t _nb_coro = (U); switch (mapi_callstate(_nb_coro)) {
 #define nb_init case (0): { mapi_continue(_nb_coro, 1);
 #define nb_state(s) goto _nb_end_leave; } case (s): { mapi_continue(_nb_coro, ((s)+1));
-#define nb_state_custom_direction(s, n) goto _nb_end_leave; } case (s): { morphine_continue(_nb_coro, (n));
+#define nb_immediately_state(s) goto _nb_end_leave; } case (s): { _nb_immediately_##s: mapi_continue(_nb_coro, ((s)+1));
 #define nb_end }} mapi_errorf(_nb_coro, "Undefined state"); _nb_end_leave: return;
 
+#define nb_immediately_continue(s) do { goto _nb_immediately_##s; } while(0)
 #define nb_continue(s) do { mapi_continue(_nb_coro, (s)); goto _nb_end_leave; } while(0)
 #define nb_leave() do { mapi_leave(_nb_coro); goto _nb_end_leave; } while(0)
 #define nb_return(x) do { { x; } mapi_return(_nb_coro); goto _nb_end_leave; } while(0)
