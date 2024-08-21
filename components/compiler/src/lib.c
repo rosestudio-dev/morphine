b301a1f12dbb8cdc66b57b4399c7ef24cd6f3225
why-iskra <v_iskortsev@mail.ru>
2024-08-21 20:15:04 +0300

    improve compiler.disassembly

--- components/compiler/src/lib.c
+++ components/compiler/src/lib.c
@@ -44,32 +44,42 @@ static void disassembly(morphine_coroutine_t U) {
     maux_nb_function(U)
         maux_nb_init
             maux_expect_args(U, 2);
+
+            mapi_push_table(U);
+
             mapi_push_arg(U, 1);
+            mapi_extract_callable(U);
+            mapi_rotate(U, 2);
+            mapi_pop(U, 1);
 
-            ml_size count;
-            if (mapi_is_type(U, "vector")) {
-                count = mapi_vector_len(U);
-            } else {
-                mapi_push_vector(U, 1);
-                mapi_rotate(U, 2);
-                mapi_vector_set(U, 0);
-                count = 1;
-            }
+            mapi_push_nil(U);
+            mapi_table_set(U);
 
-            for (ml_size i = 0; i < count; i++) {
-                mapi_vector_get(U, i);
+            for (ml_size index = 0; index < mapi_table_len(U); index++) {
+                mapi_table_idx_keyoe(U, index);
+                for (ml_size i = 0; i < mapi_constant_size(U); i++) {
+                    mapi_constant_get(U, i);
+
+                    if (mapi_is_type(U, "function")) {
+                        mapi_peek(U, 2);
+                        mapi_rotate(U, 2);
+                        mapi_push_nil(U);
+                        mapi_table_set(U);
+                    }
+
+                    mapi_pop(U, 1);
+                }
+
                 mapi_push_arg(U, 0);
                 mapi_rotate(U, 2);
-
-                mapi_extract_callable(U);
-                mapi_rotate(U, 2);
-                mapi_pop(U, 1);
 
                 mcapi_disassembly(U);
 
-                mapi_push_arg(U, 0);
-                mapi_sio_print(U, "\n");
-                mapi_pop(U, 1);
+                if (index != mapi_table_len(U) - 1) {
+                    mapi_push_arg(U, 0);
+                    mapi_sio_print(U, "\n");
+                    mapi_pop(U, 1);
+                }
             }
             maux_nb_leave();
     maux_nb_end
