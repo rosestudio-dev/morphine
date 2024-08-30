4192cf14812b20d114f6192a7c954ccd8053ccaa
why-iskra <v_iskortsev@mail.ru>
2024-08-30 04:31:27 +0300

    fix meson build for docs

--- docs/deploy.py
+++ docs/deploy.py
@@ -1,17 +1,12 @@
+import os
 import shutil
 import subprocess
-import os
 import sys
 
 cwd = os.getcwd()
 
-gradle = shutil.which("gradle")
 npm = shutil.which("npm")
 meson = shutil.which("meson")
-
-if gradle is None:
-    print("Require gradle")
-    exit(1)
 
 if npm is None:
     print("Require npm")
@@ -20,6 +15,7 @@ if npm is None:
 if npm is None:
     print("Require meson")
     exit(1)
+
 
 def step(name, args, cwd):
     print("\u001b[32;1m-- " + name + " --\u001b[0m")
@@ -34,29 +30,40 @@ def step(name, args, cwd):
         print("\u001b[31;1m-- Failure --\u001b[0m")
         exit(1)
 
+
 def insert_gitignore(path):
     file = open(path + "/.gitignore", "w")
     file.write("*")
     file.close()
 
+
 def copy(src, dest):
     os.makedirs(os.path.dirname(dest), exist_ok=True)
     shutil.copy(src, dest)
 
-step("Install deps", [npm, "i"], "site")
 
-step("Configure morphine", [meson, "setup", "docs/wasm/buildmorphine", "-Dprefix=" + cwd + "/wasm/deps", "-Ddefault_library=static", "-Dbuildapp=disabled", "-Dbuildlibs=disabled", "--cross", "docs/wasm/cross/wasm.ini"], "..")
-step("Compile morphine", [meson, "install", "--quiet"], "wasm/buildmorphine")
-insert_gitignore("wasm/deps")
+def prepare():
+    step("Install deps", [npm, "i"], "site")
 
-step("Configure wasm", [meson, "setup", "buildwasm", "-Dprefix=" + cwd + "/wasm/out", "--cross", "cross/wasm.ini"], "wasm")
-step("Compile wasm", [meson, "install", "--quiet"], "wasm/buildwasm")
-insert_gitignore("wasm/out")
-copy("wasm/out/bin/morphine.js", "site/.vitepress/generated/morphine.js")
-copy("wasm/out/bin/morphine.wasm", "site/.vitepress/generated/morphine.wasm")
+    step("Configure morphine",
+         [meson, "setup", "docs/wasm/buildmorphine", "-Dprefix=" + cwd + "/wasm/deps", "-Ddefault_library=static",
+          "-Dbuildapp=disabled", "-Dbuildlibs=disabled", "-Doptimization=2", "-Dbuildtype=release", "--cross",
+          "docs/wasm/cross/wasm.ini"], "..")
+    step("Compile morphine", [meson, "install", "--quiet"], "wasm/buildmorphine")
+    insert_gitignore("wasm/deps")
 
-step("Generate docs", [gradle, "run", "--args", "../../components ../site/src/generated kt c"], "gen")
-copy("../changelog.md", "site/src/generated/changelog.md")
+    step("Configure wasm",
+         [meson, "setup", "buildwasm", "-Doptimization=2", "-Dbuildtype=release", "-Dprefix=" + cwd + "/wasm/out",
+          "--cross", "cross/wasm.ini"],
+         "wasm")
+    step("Compile wasm", [meson, "install", "--quiet"], "wasm/buildwasm")
+    insert_gitignore("wasm/out")
+    copy("wasm/out/bin/morphine.js", "site/.vitepress/generated/morphine.js")
+    copy("wasm/out/bin/morphine.wasm", "site/.vitepress/generated/morphine.wasm")
+
+    # step("Generate docs", [gradle, "run", "--args", "../../components ../site/src/generated kt c"], "gen")
+    copy("../changelog.md", "site/src/generated/changelog.md")
+
 
 if len(sys.argv) < 2:
     variant = "dev"
@@ -64,8 +71,10 @@ else:
     variant = sys.argv[1]
 
 if variant == "dev":
+    prepare()
     step("Deploy", [npm, "run", "docs:dev", '--'] + sys.argv[2:], "site")
 elif variant == "build":
+    prepare()
     step("Build", [npm, "run", "docs:build"], "site")
 else:
     print("\u001b[31;1mSupported variants: dev, build\u001b[0m")
