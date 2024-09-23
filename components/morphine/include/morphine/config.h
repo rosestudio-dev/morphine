7d0af58afcd6c34cdaef548ef76f5f7838bca0fa
why-iskra <v_iskortsev@mail.ru>
2024-09-24 01:30:46 +0300

    improve config.h

--- components/morphine/include/morphine/config.h
+++ components/morphine/include/morphine/config.h
@@ -9,18 +9,19 @@
 #define MORPHINE_ENABLE_JUMPTABLE
 #define MORPHINE_ENABLE_SSO
 
+// modifiers
+
+#define morphine_noret  __attribute__((noreturn))
+#define morphine_dynlib extern
+
 // tags
 
 #ifdef MORPHINE_LIBRARY
-#define MORPHINE_API extern
-#define MORPHINE_AUX extern
-#define MORPHINE_LIB extern
+#define MORPHINE_API morphine_dynlib
+#define MORPHINE_AUX morphine_dynlib
+#define MORPHINE_LIB morphine_dynlib
 #else
 #define MORPHINE_API
 #define MORPHINE_AUX
 #define MORPHINE_LIB
 #endif
-
-// attrs
-
-#define morphine_noret __attribute__((noreturn))
