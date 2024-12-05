ae440b43f317084587ccb28aa9ea1176e37990c4
why-iskra <v_iskortsev@mail.ru>
2024-12-05 07:01:31 +0300

    rework init

--- components/morphine/headers/morphine/init/instance.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by why-iskra on 30.03.2024.
-//
-
-#pragma once
-
-#include "morphine/platform.h"
-
-void initI_instance(morphine_instance_t);
--- components/morphine/src/core/instance.c
+++ components/morphine/src/core/instance.c
@@ -3,8 +3,104 @@
 //
 
 #include "morphine/core/instance.h"
-#include "morphine/init/instance.h"
+#include "morphine/object/table.h"
 #include "morphine/object/sio.h"
+#include "morphine/object/native.h"
+#include "morphine/gc/finalizer.h"
+#include "morphine/gc/control.h"
+#include "morphine/api.h"
+#include "morphine/auxiliary.h"
+#include "morphine/libs/builtin.h"
+
+static morphine_library_t (*builtins[])(void) = {
+    mlib_builtin_base,
+    mlib_builtin_value,
+    mlib_builtin_gc,
+    mlib_builtin_coroutine,
+    mlib_builtin_string,
+    mlib_builtin_table,
+    mlib_builtin_userdata,
+    mlib_builtin_vector,
+    mlib_builtin_sio,
+    mlib_builtin_binary,
+    mlib_builtin_bitwise,
+    mlib_builtin_function,
+    mlib_builtin_closure,
+    mlib_builtin_exception,
+    mlib_builtin_sharedstorage,
+    mlib_builtin_iterator,
+};
+
+static void library(morphine_coroutine_t U) {
+    maux_nb_function(U)
+        maux_nb_init
+            maux_expect_args(U, 1);
+            mapi_push_arg(U, 0);
+
+            const char *name = mapi_get_cstr(U);
+            maux_library_access(U, name);
+            maux_nb_return();
+    maux_nb_end
+}
+
+static void init_sio(morphine_instance_t I) {
+    I->sio.io = sioI_create(I, I->platform.sio_io_interface);
+    I->sio.error = sioI_create(I, I->platform.sio_error_interface);
+
+    sioI_hold(I, I->sio.io, valueI_raw(0));
+    sioI_hold(I, I->sio.error, valueI_raw(0));
+
+    sioI_open(I, I->sio.io, I->data);
+    sioI_open(I, I->sio.error, I->data);
+}
+
+static void init_throw(morphine_instance_t I) {
+    throwI_special(I);
+}
+
+static void init_env(morphine_instance_t I) {
+    I->env = tableI_create(I);
+    tableI_mode_lock(I, I->env);
+}
+
+static void init_localstorage(morphine_instance_t I) {
+    I->localstorage = tableI_create(I);
+    tableI_mode_lock(I, I->localstorage);
+}
+
+static void init_sharedstorage(morphine_instance_t I) {
+    I->sharedstorage = tableI_create(I);
+    tableI_mode_lock(I, I->sharedstorage);
+}
+
+static void init_metatables(morphine_instance_t I) {
+    for (morphine_metatable_field_t field = MORPHINE_METATABLE_FIELDS_START;
+         field < MORPHINE_METATABLE_FIELDS_COUNT; field++) {
+        const char *name = metatableI_field2string(I, field);
+        I->metatable.names[field] = stringI_create(I, name);
+    }
+}
+
+static void init_libraries(morphine_instance_t I) {
+    size_t size = sizeof(builtins) / sizeof(builtins[0]);
+    for (size_t i = 0; i < size; i++) {
+        librariesI_load(I, builtins[i]());
+    }
+
+    struct string *name = stringI_create(I, "library");
+    struct value native = valueI_object(nativeI_create(I, name, library));
+    tableI_set(I, I->env, valueI_object(name), native);
+}
+
+static void init(morphine_instance_t I) {
+    init_sio(I);
+    init_throw(I);
+    init_env(I);
+    init_localstorage(I);
+    init_sharedstorage(I);
+    init_metatables(I);
+    init_libraries(I);
+}
 
 morphine_instance_t instanceI_open(morphine_platform_t platform, morphine_settings_t settings, void *data) {
     if (sizeof(struct instance) >= settings.gc.limit) {
@@ -35,7 +131,9 @@ morphine_instance_t instanceI_open(morphine_platform_t platform, morphine_settin
     };
 
     gcI_prototype(I, sizeof(struct instance));
-    initI_instance(I);
+    init(I);
+    gcI_init_finalizer(I);
+    gcI_enable(I);
 
     return I;
 }
